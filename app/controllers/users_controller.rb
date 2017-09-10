4e919e0ee858b4ba33fd556c0a4de11196b92409
Sabrina Jones <sabrina.jones87@gmail.com>
2017-09-10 07:58:48 +0000

    userscontroller to config.ru, create_acct.erb,signup logicto users_controler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
 #sign up
+get '/signup' do
+  if !session[:user_id]
+    erb :'users/create_acct'
+  else
+    redirect to '/workouts'
+  end
+end
+
+post '/signup' do
+  if params[:user_name] == "" || params[:password] == ""
+    redirect to '/signup'
+  else
+    @user = User.create(username: params[:username], password: params[:password])
+    session[:user_id] = @user.id
+    redirect '/plans'
+  end
+end
 
 
 
