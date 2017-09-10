72ab4461f38e6a46ce65674aa6c7ff69090e961b
Sabrina Jones <sabrina.jones87@gmail.com>
2017-09-10 09:13:40 +0000

    show.erb file and show to plans_controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -11,5 +11,15 @@ get '/plans/create_plan' do
   erb :'plans/create_plan'
 end
 
+#show plan
+get '/plans/:id' do
+  #binding.pry
+  if logged_in?
+    @plan = Plan.find_by_id(params[:id])
+    erb :'plans/show'
+  else
+    redirect to '/login'
+  end
+end
 
 end
